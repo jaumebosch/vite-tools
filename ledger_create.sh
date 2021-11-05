0fd15ecb6f80e071e2990092f49eecb83d3416bd
Jaume Bosch <hello@jaumebosch.com>
2021-11-06 00:42:58 +0100

    added ledger_create.sh

---
+++ ledger_create.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Vite Node ledger creator
+# Tested on Ubuntu 20.04
+# https://github.com/vitelabs/go-vite
+# https://vite.net
+
+mkdir ~/ledger
+tar -zcvf ~/ledger/ledger.tar.gz ~/.gvite/maindata/ledger/
