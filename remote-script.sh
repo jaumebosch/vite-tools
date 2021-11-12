72ae5779d9d074959204a4a9f70ddfeb57a90ad8
Jaume Bosch <hello@jaumebosch.com>
2021-11-12 12:49:20 +0100

    remo check

--- remote-script.sh
+++ remote-script.sh
@@ -3,7 +3,8 @@
 jq  '.Identity' ../gvite/node_config.json
 printf "Git pull....\n"
 git pull
-~/vite-tools/check_process.sh
+
+
 
 printf "\n\n"
 printf "================================================================\n"
